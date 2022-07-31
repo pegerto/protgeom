fd160cfc4198fd07631db7909992ac8811f3104d
pegerto.fernandez <pegerto.fernandez@creditkarma.com>
2022-07-31 20:02:50 +0100

    add name to protein for record

--- src/protgeom/pdb.py
+++ src/protgeom/pdb.py
@@ -8,7 +8,7 @@ from gzip import GzipFile
 def fetchPDB(name):
     name_curated = name.strip().lower()
     handler = {
-        200: lambda resp : parsePDBFile(io.TextIOWrapper(GzipFile('', 'r', 0, io.BytesIO(resp.content)))),
+        200: lambda resp : parsePDBFile(io.TextIOWrapper(GzipFile('', 'r', 0, io.BytesIO(resp.content))), name_curated),
         404: lambda _ :  (_ for _ in ()).throw( RuntimeError(f'Protein ${name_curated} not found'))
     }
     resp = requests.get(f'https://ftp.wwpdb.org/pub/pdb/data/biounit/PDB/all/{name_curated}.pdb1.gz')
@@ -18,7 +18,7 @@ def fetchPDB(name):
         raise RuntimeError(f'Unexpecter response from PDB: ${resp.status_code} fetching {name_curated}')
 
 
-def parsePDBFile(pdb_f):
+def parsePDBFile(pdb_f, name='unknown'):
     def parse_cord(line):
         #https://www.cgl.ucsf.edu/chimera/docs/UsersGuide/tutorials/pdbintro.html
         x = float(line[31:38])
@@ -33,4 +33,4 @@ def parsePDBFile(pdb_f):
 
     cords_folded = cords.reshape(-1,3)
     atom_num = cords_folded.shape[0]
-    return Protein(atom_num, cords_folded)
+    return Protein(atom_num, cords_folded, name)
